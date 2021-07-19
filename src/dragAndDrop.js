52662e73a78c43580540054af53155ae148c6a0a
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-19 15:58:07 +0200

    copy the todo list project

---
+++ src/dragAndDrop.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+import { swap, listItems, swapLocal } from './list.js';
+// ...............................
+export const addActionListner = () => {
+  const listElemenets = document.querySelectorAll('.list');
+  listElemenets.forEach((element) => {
+    element.addEventListener('dragstart', (e) => {
+      element.classList.add('ondrag');
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('id', element.id);
+      // console.log(e.dataTransfer.getData('id'));
+    });
+    element.addEventListener('drop', (e) => {
+      e.preventDefault();
+      e.dataTransfer.effectAllowed = 'move';
+      const fristId = e.dataTransfer.getData('id');
+      const secondId = element.id;
+      localStorage.setItem('list', JSON.stringify(listItems));
+      if (localStorage.length > 0) {
+        swapLocal(fristId, secondId);
+      } else {
+        swap(fristId, secondId);
+      }
+      localStorage.setItem('list', JSON.stringify(listItems));
+    });
+    element.addEventListener('dragend', () => {
+      element.classList.remove('ondrag');
+    });
+    element.addEventListener('dragover', (e) => {
+      e.preventDefault();
+    });
+  });
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './list.js';
+import './style.css';
